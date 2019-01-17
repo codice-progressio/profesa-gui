69a208d2629a4a4fb47e81a365b3ef0038b0b3ab
Angel <rafa.yael@gmail.com>
2019-01-17 12:25:37 -0600

    Almacen de boton creado.

---
+++ src/app/models/almacenDeBoton.model.ts
@@ -0,0 +1,15 @@
+export class AlmacenDeBoton {
+    constructor(
+        
+        public cantidadDeBoton?: number,
+
+        public createdAt?: Date,
+        public updatedAt?: Date,
+
+        public guardar?: boolean,
+        public trabajando?: boolean
+
+    ) {
+        
+    }
+}
