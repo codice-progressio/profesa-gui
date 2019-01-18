a5165670ac5cae8f46721a00e6cc254641a8efe7
Angel <rafa.yael@gmail.com>
2019-01-18 15:12:22 -0600

    Update laser.model.ts
    Agregadas opciones para poner inicio de trabajo.

--- src/app/models/laser.model.ts
+++ src/app/models/laser.model.ts
@@ -1,11 +1,16 @@
+import { Maquina } from "./maquina.model";
+
 export class Laser {
     constructor(
         public createdAt?: Date,
         public updatedAt?: Date,
 
-        public cantidadDeBoton?: number, 
+        public cantidadDeBoton?: number,
+        public bl?: number,
+        public maquinaActual?: Maquina,
+        
         public guardar?: boolean,
-        public trabajando?: boolean
+        public trabajando : boolean = false
     ) {
         
     }
