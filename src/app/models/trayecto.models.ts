c50cbe5fbe75b227b458e03a2579799e12c17175
Angel <rafa.yael@gmail.com>
2019-01-18 16:01:27 -0600

    Update trayecto.models.ts
    Departamentos faltantes agregados.

--- src/app/models/trayecto.models.ts
+++ src/app/models/trayecto.models.ts
@@ -3,6 +3,15 @@ import { Materiales } from './materiales.models';
 import { Transformacion } from './transformacion.models';
 import { Pulido } from './pulido.models';
 import { Seleccion } from './seleccion.models';
+import { ControlDeProduccion } from './controlDeProduccion.model';
+import { Pastilla } from './pastilla.model';
+import { Empaque } from './empaque.models';
+import { ProductoTerminado } from './productoTerminado.model';
+import { Metalizado } from './metalizado.model';
+import { Barnizado } from './barnizado.model';
+import { Burato } from './burato.model';
+import { Laser } from './laser.models';
+import { AlmacenDeBoton } from './almacenDeBoton.model';
 export class Trayecto {
     constructor(
 
@@ -13,6 +22,18 @@ export class Trayecto {
     public transformacion: Transformacion = new Transformacion() ,
     public pulido: Pulido = new Pulido() ,
     public seleccion: Seleccion = new Seleccion() ,
+
+
+
+    public controlDeProduccion: ControlDeProduccion = new ControlDeProduccion(),  
+    public pastilla: Pastilla = new Pastilla(),  
+    public empaque: Empaque = new Empaque(),  
+    public productoTerminado: ProductoTerminado = new ProductoTerminado(),  
+    public metalizado: Metalizado = new Metalizado(),  
+    public barnizado: Barnizado = new Barnizado(),  
+    public burato: Burato = new Burato(),  
+    public laser: Laser = new Laser(),  
+    public almacenDeBoton: AlmacenDeBoton = new AlmacenDeBoton(),  
 
     // public siguiente?: Departamento, 
     // public anterior?: Departamento,
