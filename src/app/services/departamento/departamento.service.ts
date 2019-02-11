b7ae0af837a72ae3759bc063c17f9e3ffcda4004
Angel <rafa.yael@gmail.com>
2019-02-11 13:38:16 -0600

    Update departamento.service.ts
    Actualizado a CRUD.

--- src/app/services/departamento/departamento.service.ts
+++ src/app/services/departamento/departamento.service.ts
@@ -7,44 +7,58 @@ import { ManejoDeMensajesService } from '../utilidades/manejo-de-mensajes.servic
 import { throwError } from 'rxjs';
 import { Departamento } from 'src/app/models/departamento.models';
 import { PreLoaderService } from 'src/app/components/pre-loader/pre-loader.service';
+import { CRUD } from '../crud';
+import { UtilidadesService, UsuarioService } from '../service.index';
+import { PaginadorService } from 'src/app/components/paginador/paginador.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DepartamentoService {
+export class DepartamentoService  extends CRUD<Departamento>{
 
   constructor(
     public http: HttpClient,
     public _msjService: ManejoDeMensajesService,
-    public _preLoaderService: PreLoaderService
-  ) { }
-  cargarDepartamentos( ) {
-    const a: number = this._preLoaderService.loading('Cargando departamentos.');
-    const url = URL_SERVICIOS + `/departamento`;
-    return this.http.get(url).pipe( 
-      map (( resp: any ) => {
-        this._msjService.ok_(resp,null, a);
-        return resp.departamentos;
-      }), 
-      catchError( err => {
-        this._msjService.err( err );
-        return throwError( err );
-      })
-    );
+    public _utiliadesService: UtilidadesService,
+    public _preLoaderService: PreLoaderService,
+    public _paginadorService: PaginadorService,
+    public _usuarioService: UsuarioService
+  ) {
+    super(http, _msjService, _utiliadesService, _preLoaderService, _paginadorService);
+    this.base =  URL_SERVICIOS + `/departamento`;
+    this.nombreDeDatos.plural = 'departamentos';
+    this.nombreDeDatos.singular = 'departamento';
+    this.urlBusqueda = '/buscar';
   }
 
-  guardarUnDepartamento( depto: Departamento ) {
-    const a: number = this._preLoaderService.loading('Guardando departamento.');
+  
+  // todo( ) {
+  //   const a: number = this._preLoaderService.loading('Cargando departamentos.');
+  //   const url = URL_SERVICIOS + `/departamento`;
+  //   return this.http.get(url).pipe( 
+  //     map (( resp: any ) => {
+  //       this._msjService.ok_(resp,null, a);
+  //       return resp.departamentos;
+  //     }), 
+  //     catchError( err => {
+  //       this._msjService.err( err );
+  //       return throwError( err );
+  //     })
+  //   );
+  // }
+
+  // guardar( depto: Departamento ) {
+  //   const a: number = this._preLoaderService.loading('Guardando departamento.');
     
-    const url = URL_SERVICIOS + `/departamento`;
-    return this.http.post( url, depto ).pipe(
-      map( (resp: any) => {
-        this._msjService.ok_(resp,null, a);
-        return resp.departamento;
-      }), catchError( err => {
-        this._msjService.err( err );
-        return throwError( err );
-      }) 
-    );
-  }
+  //   const url = URL_SERVICIOS + `/departamento`;
+  //   return this.http.post( url, depto ).pipe(
+  //     map( (resp: any) => {
+  //       this._msjService.ok_(resp,null, a);
+  //       return resp.departamento;
+  //     }), catchError( err => {
+  //       this._msjService.err( err );
+  //       return throwError( err );
+  //     }) 
+  //   );
+  // }
 }
