0adba66270dacd08972e65638ea0b9358b61a340
Rafael Ramirez <rafa.yael@gmail.com>
2019-05-24 13:35:30 -0500

    No gif No scss partes.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
       public router: Router,
       public _usuarioService: UsuarioService,
     ) {
-      this.fondoRandom = Math.floor(Math.random()*14)+1;
+      this.fondoRandom = 1;
      }
 
   ngOnInit() {
