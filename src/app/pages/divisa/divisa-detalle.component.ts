00731bba610374555e87af1ebb95d6fa96df9be7
Rafael Ramirez <rafa.yael@gmail.com>
2019-08-14 13:52:31 -0500

    ADD - Componentes divisas y proveedores

---
+++ src/app/pages/divisa/divisa-detalle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divisa-detalle',
+  templateUrl: './divisa-detalle.component.html',
+  styles: []
+})
+export class DivisaDetalleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
