73e97d2abfcad7546ef1050bcf9b9dc80461c8fb
Rafael Ramirez <rafa.yael@gmail.com>
2020-03-24 11:36:42 -0600

    Limpio en crud

--- src/app/services/cliente/cliente.service.ts
+++ src/app/services/cliente/cliente.service.ts
@@ -1,80 +1,158 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { URL_SERVICIOS } from '../../config/config';
-import { map, catchError } from 'rxjs/operators';
-import { Laser } from '../../models/laser.models';
-import { throwError } from '../../../../node_modules/rxjs';
-import { ManejoDeMensajesService } from '../utilidades/manejo-de-mensajes.service';
-import { Cliente } from 'src/app/models/cliente.models';
-import { PreLoaderService } from 'src/app/components/pre-loader/pre-loader.service';
-import { ModeloCompleto } from 'src/app/models/modeloCompleto.modelo';
-import { CRUD } from '../crud';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { URL_SERVICIOS, URL_BASE } from '../../config/config'
+import { map, catchError } from 'rxjs/operators'
+import { Laser } from '../../models/laser.models'
+import { throwError, Observable } from '../../../../node_modules/rxjs'
+import { ManejoDeMensajesService } from '../utilidades/manejo-de-mensajes.service'
+import { Cliente } from 'src/app/models/cliente.models'
+import { PreLoaderService } from 'src/app/components/pre-loader/pre-loader.service'
+import { ModeloCompleto } from 'src/app/models/modeloCompleto.modelo'
+import { CRUD } from '../crud'
 
-import { PaginadorService } from 'src/app/components/paginador/paginador.service';
-import { UtilidadesService } from '../utilidades/utilidades.service';
-import { UsuarioService } from '../usuario/usuario.service';
-
+import { PaginadorService } from 'src/app/components/paginador/paginador.service'
+import { UtilidadesService } from '../utilidades/utilidades.service'
+import { UsuarioService } from '../usuario/usuario.service'
+import { Paginacion } from 'src/app/utils/paginacion.util'
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClienteService extends CRUD<Cliente, undefined, undefined>{
-  
+export class ClienteService {
+  base = URL_BASE('cliente')
   constructor(
     public http: HttpClient,
     public msjService: ManejoDeMensajesService,
-    public _utiliadesService: UtilidadesService,
-    public _preLoaderService: PreLoaderService,
-    public _paginadorService: PaginadorService,
-    public _usuarioService: UsuarioService
-  ) {
-    super(http, msjService, _utiliadesService, _preLoaderService, _paginadorService);
-    this.base =  URL_SERVICIOS + `/cliente`;
-    this.nombreDeDatos.plural = 'clientes';
-    this.nombreDeDatos.singular = 'cliente';
-    this.urlBusqueda = '/buscar';
+    public utilidadesService: UtilidadesService,
+    public preLoaderService: PreLoaderService,
+    public paginadorService: PaginadorService,
+    public usuarioService: UsuarioService
+  ) {}
+
+  errFun(err) {
+    this.msjService.err(err)
+    return throwError(err)
   }
 
-  guardarNuevaMarcaLaser ( id: string, marca: string) {
-    const a: number = this._preLoaderService.loading('Guardando marca laser.');
-    
-    const url = URL_SERVICIOS + `/cliente/laser/${id}`;
+  findById(id: string): Observable<Cliente> {
+    return this.http.get<Cliente>(this.base.concat(`/${id}`)).pipe(
+      map(
+        (resp: any) => {
+          return resp.cliente as Cliente
+        },
+        catchError(err => this.errFun(err))
+      )
+    )
+  }
 
-    const laser = new Laser('', marca);
+  findAll(
+    paginacion: Paginacion = new Paginacion(30, 0, 1, 'nombre'),
+    filtros: string
+  ): Observable<Cliente[]> {
+    const url = this.base
+      .concat('?')
+      .concat(`desde=${paginacion.desde}`)
+      .concat(`&limite=${paginacion.limite}`)
+      .concat(`&campo=${paginacion.campoDeOrdenamiento}`)
+      .concat(`&sort=${paginacion.orden}`)
+      .concat(`&${filtros}`)
+
+    return this.http.get<Cliente[]>(url).pipe(
+      map((resp: any) => {
+        return resp.clientes as Cliente[]
+      }),
+      catchError(err => this.errFun(err))
+    )
+  }
+
+  findByTermn(
+    termino: string,
+    paginacion: Paginacion = new Paginacion(30, 0, 1, 'nombre')
+  ): Observable<Cliente[]> {
+    const url = this.base
+      .concat(`/buscar/${termino}?`)
+      .concat(`desde=${paginacion.desde}`)
+      .concat(`&limite=${paginacion.limite}`)
+      .concat(`&campo=${paginacion.campoDeOrdenamiento}`)
+      .concat(`&sort=${paginacion.orden}`)
+
+    return this.http.get<Cliente[]>(url).pipe(
+      map((resp: any) => {
+        return resp.clientes as Cliente[]
+      }),
+      catchError(err => this.errFun(err))
+    )
+  }
+
+  save(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.base, cliente).pipe(
+      map((resp: any) => {
+        this.msjService.toastCorrecto(resp.mensaje)
+        return resp.cliente as Cliente
+      }),
+      catchError(err => this.errFun(err))
+    )
+  }
+
+  update(cliente: Cliente): Observable<null> {
+    return this.http.put<Cliente>(this.base, cliente).pipe(
+      map((resp: any) => {
+        this.msjService.toastCorrecto(resp.mensaje)
+        return null
+      }),
+      catchError(err => this.errFun(err))
+    )
+  }
+
+  delete(id: string): Observable<Cliente> {
+    return this.http.delete<Cliente>(this.base).pipe(
+      map((resp: any) => {
+        this.msjService.toastCorrecto(resp.mensaje)
+        return resp.cliente
+      }),
+      catchError(err => this.errFun(err))
+    )
+  }
+
+  guardarNuevaMarcaLaser(id: string, marca: string) {
+    const a: number = this.preLoaderService.loading('Guardando marca laser.')
+
+    const url = URL_SERVICIOS + `/cliente/laser/${id}`
+
+    const laser = new Laser('', marca)
 
     return this.http.put(url, laser).pipe(
-      map( (resp: any) => {
-
-        this.msjService.ok_(resp,null, a);
-        return resp.cliente;
+      map((resp: any) => {
+        this.msjService.ok_(resp, null, a)
+        return resp.cliente
       }),
-      catchError (err => {
-        console.log('Capturando error.');
-        
-        this.msjService.err(err);
-        return throwError(err);
+      catchError(err => {
+        console.log('Capturando error.')
+
+        this.msjService.err(err)
+        return throwError(err)
       })
-    );
+    )
   }
 
-  solicitarAutorizacionDeModeloCompleto( cliente:Cliente,mc: ModeloCompleto ){
-    const a: number = this._preLoaderService.loading('Guardando solicitud de modelo completo.');
+  solicitarAutorizacionDeModeloCompleto(cliente: Cliente, mc: ModeloCompleto) {
+    const a: number = this.preLoaderService.loading(
+      'Guardando solicitud de modelo completo.'
+    )
     const datos = {
       modeloCompleto: mc._id,
-      usuarioQueSolicitaAutorizacion: this._usuarioService.usuario._id
-    };
-    const url =`${URL_SERVICIOS}/cliente/solicitarAutorizacion/modeloCompleto/${cliente._id}`;
+      usuarioQueSolicitaAutorizacion: this.usuarioService.usuario._id
+    }
+    const url = `${URL_SERVICIOS}/cliente/solicitarAutorizacion/modeloCompleto/${cliente._id}`
     return this.http.put(url, datos).pipe(
-      map((resp:any) => {
-        this.msjService.ok_(resp,null, a);
-        return resp.clientes;
+      map((resp: any) => {
+        this.msjService.ok_(resp, null, a)
+        return resp.clientes
       }),
-      catchError( err => {
-        this.msjService.err(err);
-        return throwError (err );
+      catchError(err => {
+        this.msjService.err(err)
+        return throwError(err)
       })
-    );
+    )
   }
-
- 
 }
