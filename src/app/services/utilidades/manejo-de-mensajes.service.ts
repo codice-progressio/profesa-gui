9b7fddad34cc5835ed94f87f6fce5db71e8c665f
Rafael Ramirez <rafa.yael@gmail.com>
2020-04-19 17:08:18 -0500

    Mejora de catpura de mensajes

--- src/app/services/utilidades/manejo-de-mensajes.service.ts
+++ src/app/services/utilidades/manejo-de-mensajes.service.ts
@@ -38,13 +38,16 @@ export class ManejoDeMensajesService {
       type: 'error',
       title: '¡Algo salio mal!'
     }
+    console.log(`err`, err)
 
-    if (!err.error) {
+    if (!err.error && err.status !== 403) {
       // Si el error viene del GUI no tiene la propiedad
       // error y no muestra el mensaje. Si pasa esto
       // lo capturamos aqui
       throw err
     }
+
+    datosSwal.text = err.error
 
     if (err.error.data) {
       datosSwal.text = err.error.data.mensaje
