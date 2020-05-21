3a77aefef781d3bc9cedb4fe808fc7105ddd84dc
Rafael Ramirez <rafa.yael@gmail.com>
2020-05-21 09:18:27 -0500

    Familia de procesos miestra defectos por default

--- src/app/pages/gestionDeProcesos/familia-de-procesos/familia-de-procesos-detalle.component.ts
+++ src/app/pages/gestionDeProcesos/familia-de-procesos/familia-de-procesos-detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { FamiliaDeProcesos } from '../../../models/familiaDeProcesos.model'
+import { ParametrosService } from '../../../services/parametros.service'
+import { LocalizacionDeOrdenes } from '../../../models/parametros/localizacionDeOrdenes.parametros.model'
 
 @Component({
   selector: 'app-familia-de-procesos-detalle',
@@ -10,6 +12,20 @@ export class FamiliaDeProcesosDetalleComponent implements OnInit {
   @Input() detalle: FamiliaDeProcesos = null
 
   @Input() id: string = 'detalleModal'
+  cargando = {}
+
+  localizacionDeOrdenes: LocalizacionDeOrdenes
+  keys = Object.keys
+  constructor(public parametrosService: ParametrosService) {
+    this.cargando['cargando'] = 'Obteniendo procesos por defecto'
+    this.parametrosService.findAllLocalizacionDeOrdenes().subscribe(
+      datos => {
+        this.localizacionDeOrdenes = datos
+        delete this.cargando['cargando']
+      },
+      _ => delete this.cargando['cargando']
+    )
+  }
 
   ngOnInit() {}
 }
