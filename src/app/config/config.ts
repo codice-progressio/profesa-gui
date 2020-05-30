3f7400822e759741d6b981fa44a895a740ab5e3e
Rafael Ramirez <rafa.yael@gmail.com>
2020-05-30 09:58:18 -0500

    Configuracion para 127.0.0.1

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 // CONFIGURACIONES PARA BD DE PRUEBA
-export const URL_SERVICIOS = 'https://192.168.1.102:2999'
-export const URL_DOMINIO = '192.168.1.102:2999'
+export const URL_SERVICIOS = 'https://127.0.0.1:2999'
+export const URL_DOMINIO = '127.0.0.1:2999'
 export const URL_BASE = path => `${URL_SERVICIOS}/${path}`
