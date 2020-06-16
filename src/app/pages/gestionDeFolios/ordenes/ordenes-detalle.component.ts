7dad6c8f19ace4596d421214e254aa29ff4a87d8
Rafael Ramirez <rafa.yael@gmail.com>
2020-06-16 15:46:04 -0500

    Imprecion funcionando

--- src/app/pages/gestionDeFolios/ordenes/ordenes-detalle.component.ts
+++ src/app/pages/gestionDeFolios/ordenes/ordenes-detalle.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core'
 import { Folio } from 'src/app/models/folio.models'
 import { FolioLinea } from '../../../models/folioLinea.models'
 import { Orden } from 'src/app/models/orden.models'
+import { ImpresionService } from '../../../services/impresion.service'
+
+declare var $: any
 
 @Component({
   selector: 'app-ordenes-detalle',
@@ -9,7 +12,7 @@ import { Orden } from 'src/app/models/orden.models'
   styles: []
 })
 export class OrdenesDetalleComponent implements OnInit {
-  constructor() {}
+  constructor(public impresionService: ImpresionService) {}
 
   @Input() folio: Folio
   @Input() linea: FolioLinea
@@ -32,7 +35,18 @@ export class OrdenesDetalleComponent implements OnInit {
     datos['idPedido'] = this.linea._id
     datos['idOrden'] = this.orden._id
     datos['cliente'] = this.folio.cliente.nombre
-    
+
     return JSON.stringify(datos)
+  }
+
+  imprimir() {
+    $('.modal').modal('hide')
+
+    setTimeout(() => {
+      this.impresionService
+        .ordenes([this.orden._id])
+        .seleccionarFolio(this.folio)
+        .imprimir()
+    }, 400)
   }
 }
