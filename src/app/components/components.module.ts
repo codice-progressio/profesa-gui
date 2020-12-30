57c3cc9002f3e6cab0fff3cc9ec3859b27663d90
Rafael Ramirez <rafa.yael@gmail.com>
2020-12-30 13:44:50 -0600

    Listado de items funcionando

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common'
 import { SkuListaComponent } from './almacen/sku-lista/sku-lista.component'
 import { SkuDetalleComponent } from './almacen/sku-detalle/sku-detalle.component'
 import { SkuCrearModificarComponent } from './almacen/sku-crear-modificar/sku-crear-modificar.component'
+import { DirectivesModule } from '../directives/directives.module'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { SkuCrearModificarComponent } from './almacen/sku-crear-modificar/sku-cr
     SkuDetalleComponent,
     SkuCrearModificarComponent
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, DirectivesModule],
   exports: [SkuListaComponent, SkuDetalleComponent, SkuCrearModificarComponent]
 })
 export class ComponentsModule {}
