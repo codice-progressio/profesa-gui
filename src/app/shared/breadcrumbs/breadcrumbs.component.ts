d81e614b9446199f9b7725164665a1b72ffdb954
Rafael Ramirez <rafa.yael@gmail.com>
2021-01-03 09:15:26 -0600

    Retorno en titulo de pagina

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivationEnd } from '@angular/router';
-import { Subscriber } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core'
+import { Router, ActivationEnd } from '@angular/router'
+import { Subscriber } from 'rxjs'
+import { filter, map } from 'rxjs/operators'
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser'
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -10,34 +11,35 @@ import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
+  titulo: string
 
-
-titulo: string;
-
-constructor( private router: Router,
-  private title: Title,
-  private meta: Meta) {
-    this.getDataRoute().subscribe( data => {
-      
-      this.titulo = data.titulo;
-      this.title.setTitle( this.titulo );
+  constructor(
+    private location: Location,
+    private router: Router,
+    private title: Title,
+    private meta: Meta
+  ) {
+    this.getDataRoute().subscribe(data => {
+      this.titulo = data.titulo
+      this.title.setTitle(this.titulo)
 
       const metaTag: MetaDefinition = {
         name: 'description',
         content: this.titulo
-      };
-      this.meta.updateTag(metaTag);
-    });
+      }
+      this.meta.updateTag(metaTag)
+    })
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  getDataRoute () {
+  getDataRoute() {
     return this.router.events.pipe(
-      filter( evento => evento instanceof ActivationEnd),
-      filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null),
-      map( (evento: ActivationEnd) => evento.snapshot.data)
-    );
+      filter(evento => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data)
+    )
   }
+
+  regresar = () => this.location.back()
 }
