15009aabff40f474f1ba7709196241dabd7c87ba
Rafael Ramirez <rafa.yael@gmail.com>
2021-01-04 13:52:02 -0600

    FIX backent URL

--- src/app/config/config.prod.ts
+++ src/app/config/config.prod.ts
@@ -1,3 +1,3 @@
 export const URL_DOMINIO = 'imperium-sic-api.herokuapp.com'
-export const URL_SERVICIOS = 'https://'.concat(URL_DOMINIO).concat('/')
+export const URL_SERVICIOS = 'https://'.concat(URL_DOMINIO)
 export const URL_BASE = path => `${URL_SERVICIOS}/${path}`
