ca889a222ca6c34c26b1914d2bb2b2af2e021213
Rafael Ramirez <rafa.yael@gmail.com>
2021-01-13 12:08:31 -0600

    Limpieza

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -7,35 +7,7 @@ import { UsuarioService } from '../usuario/usuario.service';
 export class SidebarService {
 
   public menu: any[] = [];
-  // public menu: any = [
-  //   {
-  //     titulo: 'Principal',
-  //     icono: 'mdi mdi-gauge',
-  //     submenu: [
-  //       { titulo: 'Dashboard', url: '/dashboard' },
-  //       { titulo: 'ProgressBar', url: '/progress' },
-  //       { titulo: 'Gráficas', url: '/graficas1' },
-  //       { titulo: 'Promesas', url: '/promesas' },
-  //       { titulo: 'rxjs', url: '/rxjs' },
-  //     ]
-  //   },
-  //   {
-  //     titulo: 'Mantenimientos',
-  //     icono: 'mdi mdi-folder-lock-open',
-  //     submenu: [
-  //       { titulo: 'Usuarios', url: '/usuarios' },
-  //       { titulo: 'Hospitales', url: '/hospitales' },
-  //       { titulo: 'Médicos', url: '/medicos' },
-  //     ]
-  //   },
-  //   {
-  //     titulo: 'Control de Producción',
-  //     icono: 'mdi mdi-gauge',
-  //     submenu: [
-  //       { titulo: 'Registro de folios', url: '/folios' },
-  //     ]
-  //   }
-  // ];
+  
   constructor( public _usuarioService: UsuarioService) {
   }
 
