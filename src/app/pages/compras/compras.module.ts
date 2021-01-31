3f23cfd93b27463065c5c4488d98801fe24c855c
Rafael Ramirez <rafa.yael@gmail.com>
2021-01-31 12:37:44 -0600

    Limpieza

--- src/app/pages/compras/compras.module.ts
+++ src/app/pages/compras/compras.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { Routes, RouterModule } from '@angular/router'
 import { ComprasComponent } from './compras.component'
-import { ProveedoresComponent } from './proveedores/proveedores.component'
-import { DirectivesModule } from '../../directives/directives.module'
-import { ComponentsModule } from '../../components/components.module'
-import { UxModule } from '../../ux/ux.module'
 
 const routes: Routes = [
   {
