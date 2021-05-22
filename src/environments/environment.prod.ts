0335fc6e2919340319255258e9f97af70ef857c8
Rafael Ramirez <rafa.yael@gmail.com>
2021-05-22 10:01:42 -0500

    fix ENVIROMENT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
-import { version } from '../../package.json';
+import { version } from '../../package.json'
+import { URL_DOMINIO } from '../app/config/config.prod'
 export const environment = {
   production: true,
-  VERSION: version
-};
+  VERSION: version,
+  URL_DOMINIO
+}
