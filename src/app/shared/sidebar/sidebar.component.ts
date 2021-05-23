0f3f06634c1e44b406c5bd2a43d5fee18cb3588d
Rafael Ramirez <rafa.yael@gmail.com>
2021-05-23 12:45:48 -0500

    Orden

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Usuario } from '../../models/usuario.model';
-import { SidebarService } from 'src/app/services/shared/sidebar.service';
-import { UsuarioService } from 'src/app/services/usuario/usuario.service';
+import { Component, OnInit } from '@angular/core'
+import { Usuario } from '../../models/usuario.model'
+import { SidebarService } from 'src/app/services/shared/sidebar.service'
+import { UsuarioService } from 'src/app/services/usuario/usuario.service'
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-
 export class SidebarComponent implements OnInit {
-
-  usuario: Usuario;
+  usuario: Usuario
   constructor(
     public _sideBar: SidebarService,
     public _usuarioService: UsuarioService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.usuario = this._usuarioService.usuario;
-    this._sideBar.cargarMenu();
+    this.usuario = this._usuarioService.usuario
+    this._sideBar.cargarMenu()
   }
 
   // Estos servicios los aplicamos directamente en el html.
-
 }
