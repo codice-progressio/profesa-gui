0c8ddb9e873cae614677a6f2ff481e2ae116bb58
Rafael Ramirez <rafa.yael@gmail.com>
2021-05-25 04:55:36 -0500

    Se reinician permisos de admin

--- src/app/services/instalacion.service.ts
+++ src/app/services/instalacion.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { URL_BASE } from '../config/config'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstalacionService {
-  constructor(private http: HttpClient 
-    ) {}
+  constructor(private http: HttpClient) {}
 
   base = URL_BASE('parametros')
 
@@ -15,11 +15,19 @@ export class InstalacionService {
     return this.http.post(this.base, null)
   }
 
-
   reiniciarSuperAdmin() {
-    return this.http.put<any>(
-      this.base.concat('/configurar-super-admin/permisos/reiniciar'),
-      {}
-    )
+    let _id = localStorage.getItem('id')
+
+    return this.http
+      .put<any>(URL_BASE('usuario/restaurar-permisos-administrador'), { _id })
+      .pipe(
+        map(x => {
+          localStorage.removeItem("token")
+          localStorage.removeItem("menu")
+          localStorage.removeItem("usuario")
+          localStorage.removeItem("id")
+          return x
+        })
+      )
   }
 }
