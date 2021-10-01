5b3f7b5830f2badb2d5366f20a6b73705999ca4a
Rafael Ramirez <rafa.yael@gmail.com>
2021-09-30 19:59:29 -0500

    Versionando simple funcionando

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Usuario } from '../../models/usuario.model';
-import { Router } from '@angular/router';
-import { UsuarioService } from 'src/app/services/usuario/usuario.service';
+import { Component, OnInit } from '@angular/core'
+import { Usuario } from '../../models/usuario.model'
+import { Router } from '@angular/router'
+import { UsuarioService } from 'src/app/services/usuario/usuario.service'
+import { VersionAppService } from 'src/app/services/version-app.service'
 
 @Component({
   selector: 'app-header',
@@ -9,22 +10,26 @@ import { UsuarioService } from 'src/app/services/usuario/usuario.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  anio = new Date()
+  usuario: Usuario
 
-  usuario: Usuario;
+  gui: string
+  api: string
   constructor(
+    private versionAppService: VersionAppService,
     public _usuarioService: UsuarioService,
     public router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.usuario = this._usuarioService.usuario;
+    this.usuario = this._usuarioService.usuario
+    this.versionAppService.ui.subscribe(v=>this.gui = v)
   }
 
   // Para el loguot solo ocupamos llamar el servicio de usuario que lo contiene
   // y poner la llamada directamente en el html.
 
-  buscar( termino: string ) {
-    this.router.navigate(['/busqueda', termino]);
+  buscar(termino: string) {
+    this.router.navigate(['/busqueda', termino])
   }
-
 }
