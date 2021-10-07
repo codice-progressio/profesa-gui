0037fd85f5a3152bc629be99c5834f0b1e7a5028
Rafael Ramirez <rafa.yael@gmail.com>
2021-10-07 13:41:39 -0500

    Actualizado guard a estructura nueva de permisos

--- src/app/services/guards/admin.guard.ts
+++ src/app/services/guards/admin.guard.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { UsuarioService } from '../usuario/usuario.service';
-import { ManejoDeMensajesService } from '../utilidades/manejo-de-mensajes.service';
-import { _ROLES } from '../../config/roles.const';
-
-
+import { Injectable } from '@angular/core'
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router'
+import { UsuarioService } from '../usuario/usuario.service'
+import { ManejoDeMensajesService } from '../utilidades/manejo-de-mensajes.service'
+import permisosKeysConfig from 'src/app/config/permisosKeys.config'
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AdminGuard implements CanActivate {
-
-  constructor (
+  constructor(
     public _usuarioService: UsuarioService,
     public _msj: ManejoDeMensajesService,
     public router: Router
   ) {}
 
   canActivate() {
+    let administrador = permisosKeysConfig.administrador
+    let permisosUsuario = this._usuarioService.usuario.permissions
+    if (permisosUsuario.includes(administrador)) return true
 
-    if ( 
-      this._usuarioService.usuario.permissions.includes(_ROLES.ADMIN_ROLE) ||
-      this._usuarioService.usuario.permissions.includes(_ROLES.SUPER_ADMIN) 
-      ) {
-      return true;
-    }
-    this._msj.invalido('No estas autorizado para entrar a este contenido.', 'No tienes los permisos necesarios.', 5000);
-    return false;
+    this._msj.invalido(
+      'No estas autorizado para entrar a este contenido.',
+      'No tienes los permisos necesarios.',
+      5000
+    )
+    return false
   }
-
-
 }
