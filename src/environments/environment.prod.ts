dc00f48e199fcc219b1f810be9a3ab421e86bfda
Rafael Ramirez <rafa.yael@gmail.com>
2023-06-19 14:14:26 -0600

    fix: Dominio mal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
-const URL_DOMINIO = 'profesa.app:43003'
+const URL_DOMINIO = 'profesa.app:34433'
+
 
 export const environment = {
   production: true,
   URL_DOMINIO,
-  URL_SERVICIOS: `https://${URL_DOMINIO}`
+  URL_SERVICIOS: `https://${URL_DOMINIO}/api`
 }
